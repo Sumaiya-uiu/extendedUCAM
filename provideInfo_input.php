4f12c51fb60b84b055085a20bf82461c8bc80638
[Juairia] <[juairia191002@bscse.uiu.ac.bd]>
2022-12-03 01:45:56 +0600

    provide info page added

---
+++ provideInfo_input.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <body> 
+    <body style="background-color:Gray;"></body>
+    </br>   
+    </br>
+
+	<h1>Provide Information</h1>
+
+
+
+<form method=get action=provideInfo_result.php>
+
+	Student ID : <input type=text name=student_id> <br>
+
+	<p>
+
+	Course Code : <input type=text name=course_code> <br>
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
+
+  </body>
+</html>
+
