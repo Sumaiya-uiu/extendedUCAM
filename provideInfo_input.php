ae8359fc458a0b02d79ec48c80369f679828816c
[Juairia] <[juairia191002@bscse.uiu.ac.bd]>
2022-12-04 00:23:18 +0600

    updated alignmanent

--- provideInfo_input.php
+++ provideInfo_input.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html>
   <body> 
-    <body style="background-color:Gray;"></body>
+    <body style="background-color:Thistle;"></body>
     </br>   
     </br>
-
+	<style>
+		h1 {text-align: center;}
+		h3 {text-align: center;}
+		p {text-align: center;}
+		div {text-align: center;}
+	</style>
+	
 	<h1>Provide Information</h1>
 
 
 
 <form method=get action=provideInfo_result.php>
-
+<h3>
 	Student ID : <input type=text name=student_id> <br>
 
 	<p>
@@ -19,8 +25,8 @@
 
 	<p>
 
-	<input type=submit value=submit>
-
+	<h3><input type=submit value=Submit>
+</h1>
 </form>
 
   </body>
